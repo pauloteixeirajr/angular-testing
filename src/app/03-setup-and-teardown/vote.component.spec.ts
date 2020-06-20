8a4cf33a92bf6be0ab8d7b1593336b963248320a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-20 13:50:22 +0100

    testing angular components

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,24 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+    // Triple A structure (Arrange, Act, Assert)
+    component.upVote();
+
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement totalVotes when downvoted', () => {
+    //Act
+    component.downVote();
+
+    //Assert
+    expect(component.totalVotes).toBe(-1);
   });
 });
