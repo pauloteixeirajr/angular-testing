6a939c2eb57cb8e65de92cbbab8fb5cb3117837d
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-20 14:10:28 +0100

    mocking services with spies

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,25 @@
-import { TodosComponent } from './todos.component'; 
-import { TodoService } from './todo.service'; 
+import { TodosComponent } from './todos.component';
+import { TodoService } from './todo.service';
+import { from } from 'rxjs';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
   });
 
-  it('', () => {
+  it('should set todos property with items returned from the server', () => {
+    let todos = [1, 2, 3];
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return from([todos]);
+    });
+
+    component.ngOnInit();
+
+    expect(component.todos.length).toBe(3);
+    expect(component.todos).toBe(todos);
   });
 });
