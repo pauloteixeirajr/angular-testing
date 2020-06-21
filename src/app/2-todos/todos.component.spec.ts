29a608dcc562dc5cc171f6a4e258fc91754130dc
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-21 15:45:54 +0100

    dealing with asynchronous operations

--- src/app/2-todos/todos.component.spec.ts
+++ src/app/2-todos/todos.component.spec.ts
@@ -1,5 +1,11 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -32,13 +38,15 @@ describe('TodosComponent', () => {
     component = fixture.componentInstance;
   });
 
-  it('should load todos from the server', () => {
+  it('should load todos from the server', fakeAsync(() => {
     const service = TestBed.get(TodoService);
 
-    spyOn(service, 'getTodos').and.returnValue(from([[1, 2, 3]]));
+    spyOn(service, 'getTodosPromise').and.returnValue(
+      Promise.resolve([1, 2, 3])
+    );
 
     fixture.detectChanges();
-
+    tick();
     expect(component.todos.length).toBe(3);
-  });
+  }));
 });
--- src/app/2-todos/todos.component.ts
+++ src/app/2-todos/todos.component.ts
@@ -13,7 +13,8 @@ export class TodosComponent implements OnInit {
   constructor(private service: TodoService) {}
 
   ngOnInit() {
-    this.service.getTodos().subscribe(t => (this.todos = t));
+    // this.service.getTodos().subscribe(t => (this.todos = t));
+    this.service.getTodosPromise().then(t => (this.todos = t));
   }
 
   add() {
