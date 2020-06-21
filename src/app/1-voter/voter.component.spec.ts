7c3e6568320ff9fa6de36319a70d4bf1ed28fabe
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-21 13:28:28 +0100

    basic setup for writing integration tests

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,10 +1,20 @@
 import { VoterComponent } from './voter.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 
 describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [VoterComponent],
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement;
+    // fixture.debugElement
   });
 
-  it('', () => {
-  });
+  it('', () => {});
 });
