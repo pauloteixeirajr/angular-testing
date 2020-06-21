f46fdb52e594c12e6f272a6d38e12152de305920
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-21 13:47:50 +0100

    testing property and class bindings

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,5 +1,6 @@
 import { VoterComponent } from './voter.component';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 describe('VoterComponent', () => {
   let component: VoterComponent;
@@ -16,5 +17,23 @@ describe('VoterComponent', () => {
     // fixture.debugElement
   });
 
-  it('', () => {});
+  it('should render totalVotes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    const el = de.nativeElement as HTMLElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('it should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(de.classes['highlighted']).toBeTruthy();
+  });
 });
