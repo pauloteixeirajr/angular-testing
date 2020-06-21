9d05b03222191877d9139876118e286df74103a3
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-21 15:32:04 +0100

    testing attribute directives!

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -2,29 +2,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HighlightDirective } from './highlight.directive';
 import { By } from '@angular/platform-browser';
-import { Component } from '@angular/core'; 
+import { Component } from '@angular/core';
 
 @Component({
   template: `
     <p highlight="cyan">First</p>
     <p highlight>Second</p>
-  `
+  `,
 })
-class DirectiveHostComponent { 
-}
+class DirectiveHostComponent {}
 
 describe('HighlightDirective', () => {
   let fixture: ComponentFixture<DirectiveHostComponent>;
-  
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DirectiveHostComponent, HighlightDirective ]
-    })
-    .compileComponents();
+      declarations: [DirectiveHostComponent, HighlightDirective],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DirectiveHostComponent);
-    fixture.detectChanges(); 
+    fixture.detectChanges();
+  });
+
+  it('should highlight the first element with cyan', () => {
+    const de = fixture.debugElement.queryAll(By.css('p'))[0];
+
+    expect(de.nativeElement.style.backgroundColor).toBe('cyan');
+  });
+
+  it('should highlight the second element with the default color', () => {
+    const de = fixture.debugElement.queryAll(By.css('p'))[1];
+    const directive = de.injector.get(HighlightDirective);
+
+    expect(de.nativeElement.style.backgroundColor).toBe(directive.defaultColor);
   });
 });
